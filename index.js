5550b0176c24e8a5f634d8ee2fe86cf508664d55
JaiGaneshCode <jaigstudent@gmail.com>
2020-11-14 15:45:24 +0530

    Update index.js
    Prod Port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const typeDefs= require('./graphql/typeDefs');
 
 const pubSub = new PubSub();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
     typeDefs,
